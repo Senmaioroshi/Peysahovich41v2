493040e0bc0cfa5e4e5437377ec96f16d4acb09d
Venera <Venera@DESKTOP-JDLJMNB>
2025-02-26 13:39:37 +0500

    Добавьте файлы проекта.

---
+++ Peysahovich41/ProductPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Peysahovich41
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+
+        public ProductPage()
+        {
+            InitializeComponent();
+            var currentProduct = Peysahovich41Entities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProduct;
+            ComboFilter.SelectedIndex = 0;
+
+            int ProductMaxRecords = 0;
+            foreach (Product product in currentProduct)
+                ProductMaxRecords++;
+            TBProductCountMaxRecords.Text = ProductMaxRecords.ToString();
+            UpdateProducts();
+        }
+
+        private void UpdateProducts()
+        {
+            var currentProduct = Peysahovich41Entities.GetContext().Product.ToList();
+
+            if (ComboFilter.SelectedIndex == 0)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0)).ToList();
+            }
+            if (ComboFilter.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 9.99)).ToList();
+            }
+            if (ComboFilter.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) <= 14.99)).ToList();
+            }
+            if (ComboFilter.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15)).ToList();
+            }
+            currentProduct = currentProduct.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderByDescending(p => p.ProductCost).ToList();
+            }
+
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderBy(p => p.ProductCost).ToList();
+            }
+            ProductListView.ItemsSource = currentProduct;
+            int ProductcountRecords = 0;
+            foreach (Product product in currentProduct)
+                ProductcountRecords++;
+            TBProductCountRecords.Text = ProductcountRecords.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void ComboFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+    }
+}   
